14f038a4dfb9d396cffb67d6725951097f23f8ae
ideas-webdesign <ideias.webdesign@gmail.com>
2020-03-18 10:48:27 +0000

    serve commit

--- src/App.js
+++ src/App.js
@@ -36,10 +36,8 @@ class App extends Component {
     );
     return (
       <div className="App">
-        <SearchBox 
-          placeholder='search monsters'
-          handleChange={this.handleChange}
-        />
+        <h1> Monsters Rolodex </h1>
+        <SearchBox placeholder='search monsters' handleChange={this.handleChange}/>
         <CardList monsters={filteredMonsters} />
       </div>
     );
